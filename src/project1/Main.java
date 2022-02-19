ba76753d91e7d692d1baa798c812ff37ee88b94d
Zairian <oskar.karlsson123@outlook.com>
2022-02-19 17:30:04 +0100

    Renaming of file Testington.java -> Main.java. Code formatting and better printlines

--- src/project1/Main.java
+++ src/project1/Main.java
@@ -5,31 +5,36 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class Testington {
+public class Main {
     private static final BigInteger TWO = new BigInteger("2");
     public static void main(String[] args) {
-        //BigInteger integerToBeTested = new BigInteger("531137992816767098689588206552468627329593117727031923199444138200403559860852242739162502265229285668889329486246501015346579337652707239409519978766587351943831270835393219031728127");
+        int bitSize = 512;
         int iterations = 20;
         int amountPrimesToGenerate = 100;
+
+        Random rand = new Random();
 
         RabinMiller rabinMiller = new RabinMiller();
         ArrayList<BigInteger> primes = new ArrayList<>();
 
+        System.out.println("What generation do you want to execute: \n" +
+                "1: Generation of 100 primes of selectable bitsize\n" +
+                "2: Generation of 2 primes of bitsize 512 for RSA");
+
         long timeStart = System.nanoTime();
         while(primes.size() < amountPrimesToGenerate){
-            Random rand = new Random();
-            BigInteger testPrime = new BigInteger(512, rand);
-            if(testPrime.mod(TWO).equals(BigInteger.valueOf(0))){ //Check if number is odd
+            BigInteger testPrime = new BigInteger(bitSize, rand);
+            if(testPrime.mod(TWO).equals(BigInteger.valueOf(0))){ //Check if number is even
                 continue;
             }
             if(rabinMiller.calculate(testPrime, iterations)){
                 primes.add(testPrime);
             }
         }
+
         long timeEnd = System.nanoTime();
+        System.out.println("Primes generated: " + primes.size() + " primes of bitsize " + bitSize);
         System.out.println("Time Elapsed: " + TimeUnit.NANOSECONDS.toSeconds((timeEnd-timeStart)) + "s");
         System.out.println("Average calculation time of 1 prime: " + TimeUnit.NANOSECONDS.toMillis((timeEnd-timeStart)/amountPrimesToGenerate) + "ms");
-
-        System.out.println("Primes generated: " + primes.size());
     }
 }
